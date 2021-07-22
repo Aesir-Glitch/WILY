5d63ea9ad3ab6b82099fe47cbadcab4d1a96d078
Aesir-Glitch <neel.kasaraneni@gmail.com>
2021-07-22 10:23:07 -0500

    Initial Commit

---
+++ Screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import react from "react"
+import React from "react"
+import {Text, View, StyleSheet} from "react-native"
+
+export default class SearchScreen extends React.Component {
+    render() {
+        return(
+            <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+                <Text>
+                    Search
+                </Text>
+            </View>
+        )
+    }
+}
